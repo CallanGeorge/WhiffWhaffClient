5cf396c134828c49060ebde9b4aebf5113bc1784
CallanGeorge <callangeorge19@gmail.com>
2023-04-10 15:50:43 +0100

    removed redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
+
 import { configureStore } from "@reduxjs/toolkit";
-
-import AuthReducer from "./redux/AuthReducer";
-
-const store = configureStore(AuthReducer : ()=> void);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+root.render(<App />);
