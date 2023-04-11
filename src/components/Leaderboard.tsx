ebf6d843593e2791c68d9709ec6a5dbb9de4a806
CallanGeorge <callangeorge19@gmail.com>
2023-04-11 19:58:50 +0100

    home page leaderboard displays users from db

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,22 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import css from "./Leaderboard.module.css";
 
 const Leaderboard = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/api/v1/users?page=0&size=5")
+      .then((response) => {
+        setData(response.data.content);
+      });
+  }, []);
+
   return (
     <div className={css.leaderboard}>
-      <div className={css.leaderboardUser}>
-        <div>Pic</div>
-        <div>Callan George</div>
-        <div>100</div>
-      </div>
-      <div className={css.leaderboardUser}>
-        <div>Pic</div>
-        <div>Callan George</div>
-        <div>100</div>
-      </div>
-      <div className={css.leaderboardUser}>
-        <div>Pic</div>
-        <div>Callan George</div>
-        <div>100</div>
+      {data.map((d: any) => (
+        <div className={css.leaderboardUser}>
+          <div>{d!.id}</div>
+          <div>{d!.username}</div>
+          <div>100</div>
+        </div>
+      ))}
+      <div className={css.view}>
+        <h2>View Full leaderboard</h2>
       </div>
     </div>
   );
