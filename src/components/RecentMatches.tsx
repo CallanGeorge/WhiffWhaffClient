745c3d96b80d93e9fd186a1d120037863df8414b
CallanGeorge <callangeorge19@gmail.com>
2023-04-20 16:09:35 +0100

    recent match pulls from db

--- src/components/RecentMatches.tsx
+++ src/components/RecentMatches.tsx
@@ -1,13 +1,39 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import css from "./RecentMatches.module.css";
 
 const RecentMatches = () => {
+  const [matches, setMatches] = useState<any>([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/v1/all-matches?page=0&size=5`)
+      .then((response) => {
+        console.log(response.data.content);
+        setMatches(response.data.content);
+      });
+  }, []);
   return (
     <div className={css.recentMatches}>
-      <div className={css.match}>
-        <div className={css.winner}>Callan</div>
-        <div>vs</div>
-        <div className={css.loser}>Frankie</div>
-      </div>
+      {matches ? (
+        matches.map((match: any) => (
+          <div className={css.match}>
+            <div className={css.winner}>
+              {match?.player1 === match?.winner
+                ? match?.player1
+                : match?.player2}
+            </div>
+            <div>vs</div>
+            <div className={css.loser}>
+              {match?.player1 === match?.winner
+                ? match?.player2
+                : match?.player1}{" "}
+            </div>{" "}
+          </div>
+        ))
+      ) : (
+        <span>No matches to show</span>
+      )}
     </div>
   );
 };
